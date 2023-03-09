added fk. need to fix space jacobian

kinematics/transformations.py
@@ -47,10 +47,6 @@ def check_rotation_matrix(mtx):
         return True
     
 
-
-
-
-
 def mat_exp_se3_from_units(theta, w, v):
     """
     Returns the matrix exponential of an se(3) representation (se(3) to SE(3))
@@ -96,7 +92,7 @@ def twist_to_se3(twist):
     """
     mat_se3 = np.zeros((4,4))
     mat_se3[0:3,0:3] = skew_symmetric(twist[:3])
-    mat_se3[0:3, 3] = twist[3:6]
+    mat_se3[:3, 3] = twist[3:6]
     return mat_se3
 
 def se3_to_twist(mat_se3):
@@ -112,10 +108,24 @@ def HT_to_RP(T):
 
 def RP_to_HT(R, p):
     T = np.zeros((4,4))
-    T[0: 3, 0: 3] = R
-    T[0: 3, 3] = p
+    T[:3, :3] = R
+    T[:3, 3] = p
     T[3,3] = 1
     return T
+
+
+
+def adjoint(ht):
+    """
+    Computes the adjoint representation (6x6) of an homogeneous transformation matrix (SE(3))
+    """
+    R, p = HT_to_RP(ht)
+    adj = np.zeros((6,6))
+    adj[:3,:3] = R
+    adj[3:,3:] = R
+    adj[3:,:3] = skew_symmetric(p) @ R
+
+    return adj
 
 
 

test_poe.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 from kinematics.transformations import mat_exp_se3
+
 import modern_robotics as mr 
 """
 Exemple 
@@ -23,9 +24,9 @@ S3 = np.array([1,0,0,0,2,-2])
 
 # Joint value µ
 # TODO something is wrong here
-q1 = 0*np.pi/180
-q2 = 45*np.pi/180
-q3 = 45*np.pi/180
+q1 = 75*np.pi/180
+q2 = 32*np.pi/180
+q3 = 28*np.pi/180
 
 print("Twists")
 V1=S1*q1
@@ -103,5 +104,13 @@ print("Check MR - Tb")
 print(Tb_mr)
 
 ax.scatter(Tb_mr[0,3],Tb_mr[1,3],Tb_mr[2,3],c='g', marker='o', s=50)
+
+
+from kinematics.kinematics import space_forward_kinematics as fk
+
+V = np.array([V1,V2,V3])
+
+print((Tb_mr-fk(M, V)).round())
+
 
 plt.show()